bcce70a11f7229019a4ba761e0ab9a2dc83bd452
Fishgoggles <49248928+Fishgoggles@users.noreply.github.com>
2022-11-14 00:34:25 +0500

    Add files via upload

---
+++ scripts/ShootingStars.js
@@ -0,0 +1,2 @@
+var images = ['image1.jpg', 'image2.jpg', 'image3.jpg', 'image4.jpg', 'image5.jpg'];
+$('html').css({'background-image': 'url(images/' + images[Math.floor(Math.random() * images.length)] + ')'});
